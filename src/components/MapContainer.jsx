f8a501cbc760a52242b0ca82de5fdd1bb4045f39
Yoan Malaver <yoanmalaver@gmail.com>
2020-10-03 03:32:42 -0500

    First Resolve

--- src/components/MapContainer.jsx
+++ src/components/MapContainer.jsx
@@ -1,20 +1,53 @@
 import React from 'react';
 import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
+import '../styles/containers/App.styl';
 
-const MapContainer = ({ google }) => {
-  return (
-    <Map
-      google={google}
-      zoom={5}
-      initialCenter={{ lat: 19.5943885, lng: -97.9526044 }}
-    >
-      <Marker
-        position={{ lat: 19.4267261, lng: -99.1718706 }}
-      />
-    </Map>
-  );
+class MapContainer extends React.Component {
+  state = {
+    show: false,
+  };
+
+  handleClick = () => {
+    const { show } = this.state;
+    this.setState({ show: !show });
+  };
+
+  render() {
+    const { google } = this.props;
+    const { show } = this.state;
+    if (show) {
+      return (
+        <>
+          <button
+            className="style__button-hide"
+            type="button"
+            onClick={this.handleClick}
+          >
+            No Mostrar
+          </button>
+          <Map
+            google={google}
+            zoom={5}
+            initialCenter={{ lat: 19.5943885, lng: -97.9526044 }}
+          >
+            <Marker position={{ lat: 19.4267261, lng: -99.1718706 }} />
+          </Map>
+        </>
+      );
+    } else {
+      return (
+        <button
+          className="style__button-show"
+          type="button"
+          onClick={this.handleClick}
+        >
+          Mostrar
+        </button>
+      );
+    }
+  }
 }
 
 export default GoogleApiWrapper({
-  apiKey: 'AIzaSyCmjvkXB_DMnBUNwxQztLMStyQmA_szbNw'
+  apiKey: 'AIzaSyCmjvkXB_DMnBUNwxQztLMStyQmA_szbNw',
 })(MapContainer);
